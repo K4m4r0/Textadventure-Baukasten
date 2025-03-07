32bd5ae700acc1489a9dcdee949d295384027c4e
K4m4r0 <90042720+K4m4r0@users.noreply.github.com>
2025-03-07 09:29:48 +0100

    Update locations.py

--- locations.py
+++ locations.py
@@ -9,13 +9,14 @@ def start(game):
         print("Die Sonne scheint steil von oben auf dich herab. Es scheint wohl um die Mittagszeit zu sein.")
         print("Du kannst dich an nichts erinnern. Was ist in der letzten Nacht passiert? Nur ein blaues Licht scheint dir als Erinnerung geblieben zu sein.")
         print("Als du in deine Tasche greifst, ziehst du einen Ausweis heraus.")
+        print("Auf diesem Dokument liest du 'Vereinigte Europäische Föderation'. Neben dem Schriftzug prangt das Logo der New World Order.")
         game.player = input("Du siehst auf das Foto und erkennst dein Gesicht. Daneben steht dein Name: ")
         print(f"Du denkst dir nur 'Guten Morgen, {game.player}' und siehst dich weiter um.\n")
         ausweis = inventar.Item("Personalausweis", "Dein Personalausweis mit einem Foto.")
         game.inventar_spieler.add_item(ausweis)
 
         game.current_location = location_registry["location_wald"]
-        print("Du bist in einem düsteren Wald. Hinter dir ist eine strahlende Ebene.")
+        print("Du bist in einem düsteren Wald. Hinter dir ist eine strahlende Ebene. Am Eingang zum Wald steht ein Oger.")
 
 
 def location_ebene(game, verb=None, target=None, preposition=None):
@@ -23,7 +24,7 @@ def location_ebene(game, verb=None, target=None, preposition=None):
         print("Du befindest dich in einer weiten Ebene. Vor dir ist ein Wald. Rechts neben dir ist eine Mauer mit einem Tor.")
     elif verb == "gehe" and target == "wald":
         game.current_location = location_registry["location_wald"]
-    elif verb == "gehe" and target == "tor":
+    elif verb == "gehe" and target == "tor" or target == "mauer":
         game.current_location = location_registry["tor"]
     else:
         print("Befehl nicht erkannt.")
@@ -31,26 +32,44 @@ def location_ebene(game, verb=None, target=None, preposition=None):
 
 def location_wald(game, verb=None, target=None, preposition=None):
     if verb is None:
-        print("Du bist in einem düsteren Wald. Hinter dir ist eine strahlende Ebene.")
+        print("Du bist in einem düsteren Wald. Hinter dir ist eine strahlende Ebene. Am Eingang zum Wald steht ein Oger.")
     elif verb == "gehe" and target == "ebene":
         game.current_location = location_registry["location_ebene"]
-    elif verb == "untersuche":
+    elif verb == "rede" and target == "oger":
+        print(f"Hallo, mein Name ist {game.player}, wer bist du?")
+        print("Oger: Mein Name ist Klack, was kann ich für dich tun?")
+        print("1. Nichts, vielen Dank.")
+        print("2. Weißt du, wie man durch das große Tor kommt?")
+        auswahl = input("Bitte die gewünschte Zahl eingeben: ")
+        if auswahl == "1":
+            print(f"{game.player}: Nichts, vielen Dank.")
+            print("Klack: Ok, bis bald.")
+        elif auswahl == "2":
+            print("Klack: „Natürlich. Hier, nimm diesen Schlüssel. Damit kannst du das Tor öffnen.“")
+            geheimer_schlüssel = inventar.Item("Geheimer Schlüssel", "Klack sagte, dass du damit das Tor öffnen kannst.", visible=True)
+            game.inventar_spieler.add_item(geheimer_schlüssel)
+            print(f"{game.player}: Danke für deine Hilfe!")
+        else:
+            print("Eingabe nicht erkannt.\n")
+    elif verb == "untersuche" and target == "wald":
             print("Der Wald ist sehr dunkel.")
+    elif verb == "untersuche" and target == "oger":
+        print("Ein großer, grüner Oger, der unerwartet freundlich aussieht.")
     else:
         print("Befehl nicht erkannt.")
 
 
 def tor(game, verb=None, target=None, preposition=None):
     if verb is None and game.inventar_spieler.has_item("Geheimer Schlüssel"): # Hier wird kontrolliert, ob ein Spieler ein gewisses Item im Inventar hat, um weiter zukommen. hat der Spieler das Item im Inventar, so erhält er einen anderen Text und andere Auswahlmöglichkeiten. Wichtig: name des Gegenstandes muss der aktuelle Name sein, nicht der Name der Variablen.
-        print("Mit deinem Schlüssel öffnest du die schwere Tür und kannst passieren. Du siehst einen Wald. Hinter dir ist eine Ebene.")
+        print("Mit deinem Schlüssel öffnest du die schwere Tür und kannst passieren. Vor dir liegt ein Wald. Hinter dir erstreckt sich eine Ebene.")
     elif verb == "gehe" and target == "wald" and game.inventar_spieler.has_item("Geheimer Schlüssel"): # Der andere Weg muss ebenfalls mit der Bedingung des Gegenastandes verknüpft sein.
         game.current_location = location_registry["location_wald"]
     elif verb is None:
-        print("Du stehst vor einer großen verschlossenen Tür. Hier geht es vorerst nicht weiter. Hinter dir ist eine Ebene.")
+        print("Du stehst vor einer großen, verschlossenen Tür. Hier geht es vorerst nicht weiter. Hinter dir liegt eine Ebene.")
     elif verb == "gehe" and target == "ebene":
         game.current_location = location_registry["location_ebene"]
-    elif verb == "untersuche":
-            print("Der Wald ist sehr dunkel.")
+    elif verb == "untersuche" and target == "tor":
+            print("Ein großes, altes Tor. Es hat eine Öffnung für einen Schlüssel in der Mitte.")
     else:
         print("Befehl nicht erkannt.")
 
@@ -63,3 +82,4 @@ location_registry = {
     "tor": tor
 
 }
+
